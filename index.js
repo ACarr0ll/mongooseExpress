2d17ba137a52e9503fbde555d37e778810d5ace0
ACarr0ll <carroll.andrew@gmail.com>
2022-04-23 21:32:22 -0400

    Styling changes. Adding YesYear as home page

--- index.js
+++ index.js
@@ -62,10 +62,6 @@ app.delete('/post/:id', async (req, res) => {
   res.redirect('/posts')
 })
 
-app.use((req, res) => {
-  res.status(404).send("Not Found - 404")
-})
-
 app.listen(process.env.PORT || 3000, () => {
   console.log("App is listening on port 3000")
 })
